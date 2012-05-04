430b7c4c7bac61d8f0604bed9ea8261396fe6901
Ramiro Jr. Franco <rjfranco@gmail.com>
2012-05-04 15:06:04 -0700

    Adding a yellow background collor object to the window background color. Seems confusing, but whatever.

--- Magic8Ball/app/app_delegate.rb
+++ Magic8Ball/app/app_delegate.rb
@@ -2,7 +2,9 @@ class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
     # A standard line in Objectice C
     # UIWindow *window = [[UIWindow alloc] initWithFrame:frame];
-    @window = UIWindow.alloc.initWithFrame(frame)
+    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+    @window.backgroundColor = UIColor.yellowColor
+    @window.makeKeyAndVisible
     true
   end
 end
